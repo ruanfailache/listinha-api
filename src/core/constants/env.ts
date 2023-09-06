993dc33784d0dec90fdb12f26706eb1c2dc64d52
Ruan Failache <ruanfailache@gmail.com>
2023-09-05 21:32:11 -0300

    chore: configure jest

--- src/core/constants/env.ts
+++ src/core/constants/env.ts
@@ -1,6 +1,6 @@
 export const Env = {
     Database: process.env.DATABASE_URL as string,
-    Mode: process.env.MODE as "dev" | "prod" | "test",
+    NodeEnv: process.env.NODE_ENV as "dev" | "prod" | "test",
     ApiPort: Number(process.env.API_PORT),
     JwtSecretKey: process.env.JWT_SECRET_KEY as string,
 } as const;
