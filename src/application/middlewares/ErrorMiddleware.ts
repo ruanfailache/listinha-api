4563dc6fe72a14452f5920ac0f6ee7faabe96138
Ruan Failache <ruanfailache@gmail.com>
2023-09-08 21:33:01 -0300

    refactor: change ErrorMiddleware typing

--- src/application/middlewares/ErrorMiddleware.ts
+++ src/application/middlewares/ErrorMiddleware.ts
@@ -1,15 +1,15 @@
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler } from "express";
 
 import { HttpError } from "../../core/protocols/HttpError";
 
 export class ErrorMiddleware {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    middleware(error: unknown, request: Request, response: Response, next: NextFunction): void {
+    middleware: ErrorRequestHandler = (error: unknown, request, response, next): void => {
         if (error instanceof HttpError) {
             response.status(error.statusCode).send({
                 error: error.body ?? error.message,
             });
         }
         response.status(500).send();
-    }
+    };
 }
